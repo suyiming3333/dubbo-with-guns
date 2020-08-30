12f818409193eba0fd91a31256e69814b1791c8d
suyiming <suyiming3333@gmail.com>
2020-08-30 11:11:37 +0800

    api-service 添加cinema相关api

---
+++ api-service/src/main/java/com/corn/cinema/vo/AreaVO.java
@@ -0,0 +1,15 @@
+package com.corn.cinema.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class AreaVO implements Serializable {
+
+    private String areaId;
+    private String areaName;
+    private boolean isActive;
+
+
+}
